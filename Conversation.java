787a3748b096ef90358265e33864968ecebe4e7b
Etta Applefeld <eapplefeld@smith.edu>
2025-02-17 22:36:16 -0500

    2/17

--- Conversation.java
+++ Conversation.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
 
+/** 
+ * includes attributes needed for conversation, the Conversation constructor, and methods for starting the conversation, printing the transcript, and responding to the user
+ */
 class Conversation implements Chatbot {
 
+
   // Attributes 
+
+  public int Rounds;
+  public String[] transcript;
+  public int transcriptIndex;
+  Random random = new Random();
+  public String[] cannedResponses = {"That's awesome.", "Wow!!", "Oh cool, tell me more!", "Mmhmm", "For sure for sure."};
 
   /**
    * Constructor 
    */
   Conversation() {
+    this.transcript = new String[0]; //placeholder size for the array
+    this.transcriptIndex = 0;
     
   }
 
@@ -14,13 +28,41 @@ class Conversation implements Chatbot {
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    Scanner input = new Scanner(System.in);
 
+    /**
+     * asks user how many rounds of conversation they would like and scans for integer in their response
+    */
+    System.out.println("How many rounds of conversation would you like to have?");
+    int Rounds = input.nextInt();
+    input.nextLine();
+    /**
+     * Defines the array that will store the transcript. length of array based on number of rounds and hello and goodbye statements
+     */
+    transcript = new String[(Rounds +1) *2];
+    System.out.println("Hey! How's it going?");
+    transcript[transcriptIndex++] = "Hey! How's it going?"; //makes sure greeting is stored
+
+    for(int i = 0; i < Rounds; i++) {
+      String userInput = input.nextLine();
+      transcript[transcriptIndex++] = userInput;
+      String botResponse = respond(userInput);
+      System.out.println(botResponse);
+      transcript[transcriptIndex++] = botResponse;
+    }
+    System.out.println("Nice talking to you! Goodbye!");
+    transcript[transcriptIndex++] = "Nice talking to you! Goodbye!";
   }
 
   /**
-   * Prints transcript of conversation
+   * Prints transcript of conversation. includes heading to alert that this is the transcript.
    */
   public void printTranscript() {
+    System.out.println("\n TRANSCRIPT:");
+    for(int i = 0; i < transcriptIndex; i++)  {
+      System.out.println(transcript[i]);
+
+    }
 
   }
 
@@ -28,17 +70,20 @@ class Conversation implements Chatbot {
    * Gives appropriate response (mirrored or canned) to user input
    * @param inputString the users last line of input
    * @return mirrored or canned response to user input  
+   * 
+   * my program does not include mirrored responses at this point
    */
   public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+    String botResponse = cannedResponses[random.nextInt(cannedResponses.length)];
+    return botResponse;
+
   }
 
   public static void main(String[] arguments) {
-
     Conversation myConversation = new Conversation();
     myConversation.chat();
     myConversation.printTranscript();
+
 
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,16 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    I consulted with Hanna Wheeler and Tanisha Chetty. I also talked to Miriam Pennock, who took the class last semester. 
+
 - Which session(s) of TA / office hours did you attend?
+    I was planning to go to TA hours on Sunday the 16th, but the student run org that I'm on the board of had an emergency meeting and I was not able to attend. 
+
 - What are your initial impressions of Java? 
+    I think there's some weird words that mean very specific things but I definitely feel like it's starting to make sense. There are definitely some simple commands that I'm getting the hang of, but then I feel like as soon as I try to add something new in it doesn't make sense anymore. 
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    It feels like Java can have more different applications because it is object oriented. I don't really remember a lot about Python so I'm having a hard time thinking about the similarities and differences and how they would affect programming in general. I am really curious how the two languages might have different applications. 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    The advice that I would give is not to wait to start assignments. I also started looking at at the "Think Java" pdf towards the end of working on this, and it seems like it could be really helpful, so I definitely want to look at it more and use it in the future. Trying to work on this on my own didn't really work, but working with classmates was a lot more helpful. 
